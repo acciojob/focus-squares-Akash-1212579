4c25a7a74877166bfb04e5a982f4d017ef16ffe0
Akash-1212579 <dalbhanjanakash1@gmail.com>
2025-09-22 11:35:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your JS code here. If required.
+let current1 = document.getElementById("square1");
+let current2 = document.getElementById("square2");
+let current3 = document.getElementById("square3");
+
+//for square 1
+ // when mouse enters the element
+    square1.addEventListener("mouseenter", () => {
+     current2.classList.add("change-color");
+		current3.classList.add("change-color");
+    });
+
+    // when mouse leaves the element
+    square1.addEventListener("mouseleave", () => {
+     current2.classList.remove("change-color");
+		current3.classList.remove("change-color");
+    });
+
+//for square2
+
+ // when mouse enters the element
+    square2.addEventListener("mouseenter", () => {
+     current1.classList.add("change-color");
+		current3.classList.add("change-color");
+    });
+
+    // when mouse leaves the element
+    square2.addEventListener("mouseleave", () => {
+     current1.classList.remove("change-color");
+		current3.classList.remove("change-color");
+    });
+
+//for square 3
+
+ // when mouse enters the element
+    square3.addEventListener("mouseenter", () => {
+     current1.classList.add("change-color");
+		current2.classList.add("change-color");
+    });
+
+    // when mouse leaves the element
+    square3.addEventListener("mouseleave", () => {
+     current1.classList.remove("change-color");
+		current2.classList.remove("change-color");
+    });
